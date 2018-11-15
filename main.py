da639ca7638c261f23d587b448ebd98ec4456d7e
YajanaRao <yajananrao@gmail.com>
2018-11-15 16:03:39 +0530

    Chatbot with chatbot data

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
 import nltk
 from flask import Flask,jsonify,request
-app = Flask(__name__)
 import random
+import numpy as np
+import string
 
+app = Flask(__name__)
 
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 
-@app.route("/",methods=['GET', 'POST'])
-def greeting():
+f=open('chatbot.txt','r',errors = 'ignore')
+raw=f.read()
+raw=raw.lower()# converts to lowercase
+
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+
+
+lemmer = nltk.stem.WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Generating response
+def response(user_response):
+    robo_response=''
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+@app.route("/gethint.php",methods=['GET', 'POST'])
+def getHint():
+	global word_tokens
+	global sent_tokens
 	if request.method == 'GET':
-		sentence = request.args.get('q')
-		for word in sentence.split():
+		user_response = request.args.get('q')
+		for word in user_response.split():
 			if word.lower() in GREETING_INPUTS:
 				return jsonify(random.choice(GREETING_RESPONSES))
+
+			else:
+				sent_tokens.append(user_response)
+				word_tokens=word_tokens+nltk.word_tokenize(user_response)
+				final_words=list(set(word_tokens))
+				resp = response(user_response)
+				sent_tokens.remove(user_response)
+				return jsonify(resp)
