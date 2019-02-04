a2cb12a2ebda105b2d34e24f888a42a8468578cd
Yajana Rao <yajana.merahkee@gmail.com>
2019-02-04 13:09:35 +0530

    Removed video feature

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask,jsonify,request
 import random
 import numpy as np
 import string
-import cv2
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -69,25 +68,5 @@ def getHint():
 				sent_tokens.remove(user_response)
 				return jsonify(resp)
 
-@app.route("/video")
-def capture():
-    try:
-        cap = cv2.VideoCapture(0)
-        while(True):
-            # Capture frame-by-frame
-            ret, frame = cap.read()
 
-            # Our operations on the frame come here
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            # Display the resulting frame
-            cv2.imshow('frame', gray)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        # When everything done, release the capture
-        cap.release()
-        cv2.destroyAllWindows()
-    except cv2.error as e:
-        return jsonify("failed")
    
